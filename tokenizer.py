9c0efdab332fa9f9179e53494b78d07fea7db7e3
Aditya Kumar <aditya.kumar774@gmail.com>
2018-01-23 19:32:07 +0530

    More regular expressions

--- tokenizer.py
+++ tokenizer.py
@@ -4,12 +4,18 @@ from collections import Counter
 
 emoticons_str = r"""
     (?:
-       [:=;]    #Eyes
-       [oO\-]?  #Nose
-       [D\)\]\(\]/\\OpP]  #Mouth
+       [:=;]
+       [oO\-]?
+       [D\)\]\(\]/\\OpP]
     )"""
 
 regex_str = [
     emoticons_str,
-    
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # mentions
+    r"(?:\#+[\w_]+[\w\'_\-]'[\w_]+)",  # hash tags
+    r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',  # urls
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and _
+
 ]
