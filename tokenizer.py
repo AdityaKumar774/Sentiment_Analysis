a718588f78fdb498400dbbdae480a74b8df6dc63
Aditya Kumar <aditya.kumar774@gmail.com>
2018-01-24 00:24:58 +0530

    Compiling regular expression

--- tokenizer.py
+++ tokenizer.py
@@ -17,5 +17,10 @@ regex_str = [
     r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',  # urls
     r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
     r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and _
-
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # every thing else
 ]
+
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticons_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
