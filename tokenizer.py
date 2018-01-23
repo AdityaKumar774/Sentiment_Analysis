46897cbf3c4dd8800b59fe739eac3c843c713249
Aditya Kumar <aditya.kumar774@gmail.com>
2018-01-24 00:50:17 +0530

    Counting the terms

--- tokenizer.py
+++ tokenizer.py
@@ -21,6 +21,39 @@ regex_str = [
     r'(?:\S)'  # every thing else
 ]
 
-tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-emoticons_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticons_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
 
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticons_re.search(token) else token.lower() for token in tokens]
+        return tokens
+
+
+from nltk.corpus import stopwords
+import string
+
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'RT', 'via']
+fname = '/home/knight/PycharmProjects/Sentiment_Analysis/Twitter_Analysis/data.json'
+
+with open(fname, 'r', newline='\r\n') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # create a list with all the terms
+        terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+        # count terms only once, equivalent to document frequency
+        terms_single = set(terms_stop)
+        # count hash tags only
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+        # count term only (no hash tags, no mentions)
+        terms_only = [term for term in preprocess(tweet['text'].lower()) if term not in stop]
+
+        
